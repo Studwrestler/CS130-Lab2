4bc9e277bc75e3c997fa3256f65cb356cc0405dd
Studwrestler <112411998+Studwrestler@users.noreply.github.com>
2022-09-08 09:39:56 -0400

    Add files via upload

---
+++ MyMondaySchedule.java
@@ -0,0 +1,18 @@
+public class MyMondaySchedule
+{
+	public static void main(String[]args)
+	{
+		System.out.println("On Monday I wake up at "+5+" AM");
+		System.out.println("I lift at "+6+" AM");
+		System.out.println("I shower at" +7+ "AM");
+		System.out.println("I go to my first class at "+9+" AM");
+		System.out.println("I study at the library at "+10+" AM");
+		System.out.println("I attend my second class at "+11+" AM");
+		System.out.println("I eat lunch at "+12+" PM");
+		System.out.println("I go to my third class at "+1+" PM");
+		System.out.println("I go to wrestling practice at "+3+" PM");
+		System.out.println("I go to my final class at "+6+" PM");
+		System.out.println("I eat dinner at "+8+" PM");
+		System.out.println("I fall asleep at "+11+" PM");
+	}
+}
