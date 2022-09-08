e5855d5c7bd8176d46d1da263eec83a0314a2304
Studwrestler <112411998+Studwrestler@users.noreply.github.com>
2022-09-08 09:39:57 -0400

    Add files via upload

---
+++ SchoolAddress.java
@@ -0,0 +1,10 @@
+public class SchoolAddress
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Bellarmine University \n"+
+		2001+" Newburg Road\n"+
+		"Louisville,Kentucky "+40205);
+	}
+}
+		
